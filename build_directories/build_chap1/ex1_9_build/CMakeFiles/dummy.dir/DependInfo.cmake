9311095e0c4deb66f188bf8b7f3865726bb89295
codetiger7 <codetiger@icloud.com>
2016-09-28 19:20:18 +0200

    ok

---
+++ build_directories/build_chap1/ex1_9_build/CMakeFiles/dummy.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/brianopedal/git/cpp_primer_4th_edition/chap1/dummy/main.cpp" "/Users/brianopedal/Git/cpp_primer_4th_edition/build_directories/build_chap1/ex1_9_build/CMakeFiles/dummy.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
